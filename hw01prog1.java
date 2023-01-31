f021463e0e1056c0d9bf69366584a4fffc51da61
kadenkrider2 <118944863+kadenkrider2@users.noreply.github.com>
2023-01-30 20:14:57 -0500

    Create hw01prog1.java

---
+++ hw01prog1.java
@@ -0,0 +1,5 @@
+public class Hw01prog1 {
+	public static void main(String[] args) {
+		System.out.println("My name is Kaden Krider, and my age is 18");
+		System.out.println("My school is Virginia State University");
+	}
