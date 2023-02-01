d0b445b9a1ee83ae375cad3a467af6db3873543c
kadenkrider2 <118944863+kadenkrider2@users.noreply.github.com>
2023-01-31 22:22:25 -0500

    Update hw01prog1.java

--- hw01prog1.java
+++ hw01prog1.java
@@ -1,5 +1,8 @@
-public class Hw01prog1 {
-	public static void main(String[] args) {
-		System.out.println("My name is Kaden Krider, and my age is 18");
-		System.out.println("My school is Virginia State University");
-	}
+public class hw01prog1 {
+    public static void main(String[] args) {
+        String var1 = "My name is Kaden Krider, and my age is 18";
+        String var2 = "My school is Virginia State University";
+        System.out.println(var1);
+        System.out.println(var2);
+    }
+}
